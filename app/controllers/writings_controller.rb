9addb8f32056913cda997db54da00f4e09b730f3
Anna Bothe <annabothe0@gmail.com>
2023-12-22 13:59:43 +0900

    finished about page

---
+++ app/controllers/writings_controller.rb
@@ -0,0 +1,4 @@
+class WritingsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'writings/index'
+  get 'illustrations/index'
+  get 'projects/index'
   get '/', to: 'homepage#home', as: 'root'
   get 'about/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
